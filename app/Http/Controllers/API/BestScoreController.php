d443d618933ad56e5384014737e3ea1c7591a6ce
SU1_Basiir <softwareu1@i-smart.com.sg>
2023-01-05 17:35:30 +0800

    add username to the leaderboard

--- app/Http/Controllers/API/BestScoreController.php
+++ app/Http/Controllers/API/BestScoreController.php
@@ -14,9 +14,10 @@ class BestScoreController extends BaseController
     public function index()
     {
         $sums = DB::table('best_score')
-            ->select(DB::raw('sum(best_score_value) as sum , user_id' ))
+            ->select(DB::raw('sum(best_score.best_score_value) as sum , best_score.user_id, user.username as username'))
             ->groupBy('user_id')
             ->orderBy('sum', 'desc')
+            ->join('users', 'users.user_id', '=', 'best_score.user_id')
             ->get();
         return $this->sendResponse($sums, 'Best Score fetched.');
     }
@@ -37,16 +38,13 @@ class BestScoreController extends BaseController
 
     public function show($id)
     {
-
     }
 
     public function update(Request $request, Achievement $achievement)
     {
-
     }
 
     public function destroy(Achievement $achievement)
     {
-
     }
 }
